04808d8df188d2ddd1372e1797658f1334ce1bd1
Matias Berrutti <berrutti@gmail.com>
2026-04-01 23:11:20 +0200

    v1.0.3

--- scripts/build.ts
+++
@@ -1,6 +0,0 @@
-import { readFileSync, writeFileSync } from "fs";
-
-const { version } = JSON.parse(readFileSync("package.json", "utf8")) as { version: string };
-const manifest = JSON.parse(readFileSync("extension/manifest.json", "utf8")) as Record<string, unknown>;
-manifest.version = version;
-writeFileSync("extension/manifest.json", JSON.stringify(manifest, null, 2) + "\n");
